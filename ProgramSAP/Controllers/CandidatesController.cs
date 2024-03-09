578fa3914dead72ff6bc8590633a8bc6e9f6ba1b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 18:31:51 +0100

    Candidate controller with GetAll endpoint created

--- ProgramSAP/Controllers/CandidatesController.cs
+++ ProgramSAP/Controllers/CandidatesController.cs
@@ -1,5 +1,24 @@
-﻿namespace ProgramSAP.Controllers;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
-public class CandidatesController
+namespace ProgramSAP.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CandidatesController : ControllerBase
 {
+    private readonly IMediator mediator;
+
+    public CandidatesController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetAllCandidates([FromQuery] GetCandidatesRequest request)
+    {
+        var response = await this.mediator.Send(request);
+        return this.Ok(response);
+    }
 }
