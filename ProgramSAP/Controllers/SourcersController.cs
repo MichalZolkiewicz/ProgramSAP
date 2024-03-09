bfb8b10a8afe86098db33a027081b4b5688deff2
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 19:27:03 +0100

    Sourcer controller created with GetAll method

--- ProgramSAP/Controllers/SourcersController.cs
+++ ProgramSAP/Controllers/SourcersController.cs
@@ -1,5 +1,25 @@
-﻿namespace ProgramSAP.Controllers;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProgramSAP.ApplicationServices.API.Domain;
 
-public class SourcersController
+namespace ProgramSAP.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SourcersController : ControllerBase
 {
+    private readonly IMediator mediator;
+
+    public SourcersController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetAllSourcers([FromQuery] GetSourcersRequest request)
+    {
+        var response = await this.mediator.Send(request);
+        return this.Ok(response);
+    }
 }
