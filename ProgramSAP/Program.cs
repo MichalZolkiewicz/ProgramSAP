785f63814129cc47b1fb1104c7c2c9adb6e70a5b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 18:17:01 +0100

    MediatR nuget added and configured

--- ProgramSAP/Program.cs
+++ ProgramSAP/Program.cs
@@ -1,18 +1,23 @@
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using ProgramSAP.DataAccess;
 using ProgramSAP.DataAccess.Repositories;
+using ProgramSAP.ApplicationServices.API.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMediatR(typeof(ResponseBase<>));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddDbContext<RecruitingProgramContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("RecruitingProgramConnection")));
+
 
 var app = builder.Build();
 
