92fcb514aa41e245062151582752d92bf88ff764
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 19:03:01 +0100

    GetAllManagers endpoint cretaed

--- ProgramSAP/Controllers/ManagersController.cs
+++ ProgramSAP/Controllers/ManagersController.cs
@@ -1,5 +1,25 @@
-﻿namespace ProgramSAP.Controllers;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProgramSAP.ApplicationServices.API.Domain;
 
-public class ManagersController
+namespace ProgramSAP.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ManagersController : ControllerBase
 {
+    private readonly IMediator mediator;
+
+    public ManagersController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetAllManagers([FromQuery] GetManagersRequest request)
+    {
+        var response = await mediator.Send(request);
+        return this.Ok(response);
+    }
 }
