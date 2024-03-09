18a8a5998a4cb793db5c79184341bedb0c1a1a6e
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 18:57:02 +0100

    Request/Response classes created, handler created and implemented

---
+++ ProgramSAP.ApplicationServices/API/Domain/GetManagersResponse.cs
@@ -0,0 +1,7 @@
+﻿using ProgramSAP.ApplicationServices.API.Domain.Models;
+
+namespace ProgramSAP.ApplicationServices.API.Domain;
+
+public class GetManagersResponse : ResponseBase<List<Manager>>
+{
+}
---
+++ ProgramSAP.ApplicationServices/API/Handlers/GetManagersHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using ProgramSAP.ApplicationServices.API.Domain;
+using ProgramSAP.DataAccess.Entities;
+using ProgramSAP.DataAccess.Repositories;
+
+namespace ProgramSAP.ApplicationServices.API.Handlers;
+
+public class GetManagersHandler : IRequestHandler<GetManagersRequest, GetManagersResponse>
+{
+    private readonly IRepository<Manager> managerRepository;
+
+    public GetManagersHandler(IRepository<Manager> managerRepository)
+    {
+        this.managerRepository = managerRepository;
+    }
+    public Task<GetManagersResponse> Handle(GetManagersRequest request, CancellationToken cancellationToken)
+    {
+        var managers = managerRepository.GetAll();
+        var domainManagers = managers.Select(x => new Domain.Models.Manager
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Surname = x.Surname,
+        });
+
+        var response = new GetManagersResponse()
+        {
+            Data = domainManagers.ToList()
+        };
+
+        return Task.FromResult(response);
+    }
+}
