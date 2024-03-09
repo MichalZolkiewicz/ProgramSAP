a9a96e1ef49eecfccc6a8af0c7f87bbb752c9dec
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 19:04:58 +0100

    Recruiter model created

---
+++ ProgramSAP.ApplicationServices/API/Domain/Models/Recruiter.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramSAP.ApplicationServices.API.Domain.Models;
+
+public class Recruiter
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
