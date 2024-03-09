c404d12c633b1d1164192a92898fde134e3e0c43
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 18:56:27 +0100

    Manager model for response created

---
+++ ProgramSAP.ApplicationServices/API/Domain/Models/Manager.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramSAP.ApplicationServices.API.Domain.Models;
+
+public class Manager
+{   
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
