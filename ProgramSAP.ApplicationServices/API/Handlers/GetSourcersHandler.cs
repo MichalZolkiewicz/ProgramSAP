61b34fe9c101b8d9f96ed27c8f999193a0562019
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-09 19:20:59 +0100

    GetSourcers handler created and implemented

---
+++ ProgramSAP.ApplicationServices/API/Handlers/GetSourcersHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using ProgramSAP.ApplicationServices.API.Domain;
+using ProgramSAP.DataAccess.Entities;
+using ProgramSAP.DataAccess.Repositories;
+
+namespace ProgramSAP.ApplicationServices.API.Handlers;
+
+public class GetSourcersHandler : IRequestHandler<GetSourcersRequest, GetSourcersResponse>
+{
+    private readonly IRepository<Sourcer> sourcerRepository;
+
+    public GetSourcersHandler(IRepository<Sourcer> sourcerRepository)
+    {
+        this.sourcerRepository = sourcerRepository;
+    }
+    public Task<GetSourcersResponse> Handle(GetSourcersRequest request, CancellationToken cancellationToken)
+    {
+        var sourcers = sourcerRepository.GetAll();
+        var domainSourcers = sourcers.Select(x => new Domain.Models.Sourcer
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Surname = x.Surname,
+        });
+
+        var response = new GetSourcersResponse
+        {
+            Data = domainSourcers.ToList()
+        };
+
+        return Task.FromResult(response);
+    }
+}
