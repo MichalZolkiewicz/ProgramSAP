cde363f3cc8221990551c2600fffe9d47594e840
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-12 23:55:17 +0100

    handler updated to work with queryexecutor

--- ProgramSAP.ApplicationServices/API/Handlers/GetCandidatesHandler.cs
+++ ProgramSAP.ApplicationServices/API/Handlers/GetCandidatesHandler.cs
@@ -1,24 +1,25 @@
 ﻿using AutoMapper;
 using MediatR;
 using ProgramSAP.ApplicationServices.API.Domain;
-using ProgramSAP.DataAccess.Entities;
-using ProgramSAP.DataAccess.Repositories;
+using ProgramSAP.DataAccess.CQRS;
+using ProgramSAP.DataAccess.CQRS.Queries;
 
 namespace ProgramSAP.ApplicationServices.API.Handlers;
 
 public class GetCandidatesHandler : IRequestHandler<GetCandidatesRequest, GetCandidatesResponse>
 {
-    private readonly IRepository<Candidate> candidateRepository;
     private readonly IMapper mapper;
+    private readonly IQueryExecutor queryExecutor;
 
-    public GetCandidatesHandler(IRepository<Candidate> candidateRepository, IMapper mapper)
+    public GetCandidatesHandler(IMapper mapper, IQueryExecutor queryExecutor)
     {
-        this.candidateRepository = candidateRepository;
         this.mapper = mapper;
+        this.queryExecutor = queryExecutor;
     }
     public async Task<GetCandidatesResponse> Handle(GetCandidatesRequest request, CancellationToken cancellationToken)
     {
-        var candidates = await candidateRepository.GetAll();
+        var query = new GetCandidatesQuery();
+        var candidates = await this.queryExecutor.Execute(query);
         var mappedCandidates = mapper.Map<List<Domain.Models.Candidate>>(candidates);
 
         var response = new GetCandidatesResponse()
