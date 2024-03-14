71977b0774bcaf6094385b3a0711f176f4e59b70
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-15 00:33:56 +0100

    Candidate request and response created

---
+++ ProgramSAP.ApplicationServices/API/Domain/AddCandidateRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ProgramSAP.ApplicationServices.API.Domain;
+
+public class AddCandidateRequest : IRequest<AddCandidateRespone>
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
---
+++ ProgramSAP.ApplicationServices/API/Domain/AddCandidateRespone.cs
@@ -0,0 +1,8 @@
+﻿using ProgramSAP.DataAccess.Entities;
+
+namespace ProgramSAP.ApplicationServices.API.Domain;
+
+public class AddCandidateRespone : ResponseBase<Candidate>
+{
+
+}
