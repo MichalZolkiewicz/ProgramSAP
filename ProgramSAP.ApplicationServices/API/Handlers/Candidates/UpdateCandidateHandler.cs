276381b19bfc9db9c2b00e692aa8ed4760789403
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-16 00:49:08 +0100

    UpdateCandidate command and handler created

---
+++ ProgramSAP.ApplicationServices/API/Handlers/Candidates/UpdateCandidateHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.Update;
+using ProgramSAP.DataAccess.CQRS;
+using ProgramSAP.DataAccess.CQRS.Commands;
+
+namespace ProgramSAP.ApplicationServices.API.Handlers.Candidates;
+
+public class UpdateCandidateHandler : IRequestHandler<UpdateCandidateRequest, UpdateCandidateResponse>
+{
+    private readonly IMapper mapper;
+    private readonly ICommandExecutor commandExecutor;
+
+    public UpdateCandidateHandler(IMapper mapper, ICommandExecutor commandExecutor)
+    {
+        this.mapper = mapper;
+        this.commandExecutor = commandExecutor;
+    }
+
+    public async Task<UpdateCandidateResponse> Handle(UpdateCandidateRequest request, CancellationToken cancellationToken)
+    {
+        var candidate = this.mapper.Map<DataAccess.Entities.Candidate>(request);
+        var command = new UpdateCandidateCommand() { Parameter =  candidate };
+        var updatedCandidate = await commandExecutor.Execute(command);
+        return new UpdateCandidateResponse
+        {
+            Data = this.mapper.Map<Candidate>(updatedCandidate)
+        };
+    }
+}
---
+++ ProgramSAP.DataAccess/CQRS/Commands/UpdateCandidateCommand.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramSAP.DataAccess.Entities;
+
+namespace ProgramSAP.DataAccess.CQRS.Commands;
+
+public class UpdateCandidateCommand : CommandBase<Candidate, Candidate>
+{
+    public override async Task<Candidate> Execute(RecruitingProgramContext context)
+    {
+        var candidate = await context.Candidates.FirstOrDefaultAsync(x => x.Id == this.Parameter.Id);
+        if (candidate != null) 
+        {
+            candidate.Name = this.Parameter.Name;
+            candidate.Surname = this.Parameter.Surname;
+            candidate.Email = this.Parameter.Email;
+        }
+        await context.SaveChangesAsync();
+        return this.Parameter;
+    }
+}
