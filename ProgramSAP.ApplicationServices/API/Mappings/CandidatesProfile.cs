dcbf7804e818d313b978704b8df0cc56babd044c
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-16 00:48:07 +0100

    Mapping added

--- ProgramSAP.ApplicationServices/API/Mappings/CandidatesProfile.cs
+++ ProgramSAP.ApplicationServices/API/Mappings/CandidatesProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
-using ProgramSAP.ApplicationServices.API.Domain.Models;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.Add;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.Update;
 
 namespace ProgramSAP.ApplicationServices.API.Mappings;
 
@@ -7,9 +9,21 @@ public class CandidatesProfile : Profile
 {
     public CandidatesProfile()
     {
-        this.CreateMap<ProgramSAP.DataAccess.Entities.Candidate, Candidate>()
+
+        this.CreateMap<AddCandidateRequest, DataAccess.Entities.Candidate>()
+            .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
+            .ForMember(x => x.Surname, y => y.MapFrom(z => z.Surname))
+            .ForMember(x => x.Email, y => y.MapFrom(z => z.Email));
+
+        this.CreateMap<DataAccess.Entities.Candidate, Candidate>()
             .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
             .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
             .ForMember(x => x.Surname, y => y.MapFrom(z => z.Surname));
+
+        this.CreateMap<UpdateCandidateRequest, DataAccess.Entities.Candidate>()
+            .ForMember(x => x.Id, y => y.MapFrom(z => z.CandidateId))
+            .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
+            .ForMember(x => x.Surname, y => y.MapFrom(z => z.Surname))
+            .ForMember(x => x.Email, y => y.MapFrom(z => z.Email));
     }
 }
