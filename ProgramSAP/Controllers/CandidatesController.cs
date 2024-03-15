2e6aede4dd47ed2b2dce3631fad10c2ca44a687e
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-16 00:33:29 +0100

    GetById and Update endpoints added

--- ProgramSAP/Controllers/CandidatesController.cs
+++ ProgramSAP/Controllers/CandidatesController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using ProgramSAP.ApplicationServices.API.Domain;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.Add;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.GetAll;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.GetById;
+using ProgramSAP.ApplicationServices.API.Domain.Candidate.Update;
 
 namespace ProgramSAP.Controllers;
 
@@ -23,9 +26,29 @@ public class CandidatesController : ControllerBase
         return this.Ok(response);
     }
 
+    [HttpGet]
+    [Route("{candidateId}")]
+    public async Task<IActionResult> GetCandidateById([FromRoute] int candidateId)
+    {
+        var request = new GetCandidateByIdRequest
+        {
+            CandidateId = candidateId
+        };
+        var response = await this.mediator.Send(request);
+        return this.Ok(response);
+    }
+
     [HttpPost]
     [Route("")]
     public async Task<IActionResult> AddCandidate([FromBody] AddCandidateRequest request)
+    {
+        var response = await this.mediator.Send(request);
+        return this.Ok(response);
+    }
+
+    [HttpPut]
+    [Route("")]
+    public async Task<IActionResult> UpdateCandidate([FromBody] UpdateCandidateRequest request)
     {
         var response = await this.mediator.Send(request);
         return this.Ok(response);
