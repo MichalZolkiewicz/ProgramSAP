7121c8171bcdaaf2741a3e05a73fbd4bc5e02f53
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 00:54:42 +0100

    class for hash passwords

---
+++ ProgramSAP.ApplicationServices/API/Domain/Security/HashPasswordHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace ProgramSAP.Authentication;
+
+public class HashPasswordHandler
+{
+    public static string HashPassowrd(string password)
+    {
+        // generate a 128-bit salt using a secure PRNG
+        byte[] salt = new byte[128 / 8];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+
+        // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+        var storedHashPassword = Convert.ToBase64String(salt) + ";" + hashed;
+        return storedHashPassword;
+    }
+}
