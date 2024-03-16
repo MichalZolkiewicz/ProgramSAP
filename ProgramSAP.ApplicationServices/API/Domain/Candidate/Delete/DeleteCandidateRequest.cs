fb79fde11a13760b0564a62c056bf277f362e45b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-16 12:06:07 +0100

    Response and Request for DeleteCandidate created

---
+++ ProgramSAP.ApplicationServices/API/Domain/Candidate/Delete/DeleteCandidateRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ProgramSAP.ApplicationServices.API.Domain.Candidate.Delete;
+
+public class DeleteCandidateRequest : IRequest<DeleteCandidateResponse>
+{
+    public int CandidateId { get; set; }
+}
