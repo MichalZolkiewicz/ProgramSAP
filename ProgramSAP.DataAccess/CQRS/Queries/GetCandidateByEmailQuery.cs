8327ef44882632ab46527129e0797e6f6a0e7056
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 00:53:43 +0100

    query for candidate added

---
+++ ProgramSAP.DataAccess/CQRS/Queries/GetCandidateByEmailQuery.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramSAP.DataAccess.Entities;
+
+namespace ProgramSAP.DataAccess.CQRS.Queries;
+
+public class GetCandidateByEmailQuery : QueryBase<Candidate>
+{
+    public string Email { get; set; }
+    public override Task<Candidate> Execute(RecruitingProgramContext context)
+    {
+        return context.Candidates.FirstOrDefaultAsync(x => x.Email == this.Email);
+    }
+}
